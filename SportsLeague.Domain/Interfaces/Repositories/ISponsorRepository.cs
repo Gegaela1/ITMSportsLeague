edb6c6a77a89079846036524b327629153bf9733
Andrea Espinosa <gespinol1210@gmail.com>
2026-04-03 22:26:53 -0500

    Add ISponsorRepository  -SponsorRepository

---
+++ SportsLeague.Domain/Interfaces/Repositories/ISponsorRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.Interfaces.Repositories
+{
+    public interface ISponsorRepository
+    {
+    }
+}
