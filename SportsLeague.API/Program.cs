01c0949ee081e7aac0709970280ddf0cbeea1b70
Andrea Espinosa <gespinol1210@gmail.com>
2026-04-03 21:13:17 -0500

    Add services and repositories Program.cs

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -16,16 +16,19 @@ builder.Services.AddDbContext<LeagueDbContext>(options =>
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ITeamRepository, TeamRepository>();
 builder.Services.AddScoped<IPlayerRepository, PlayerRepository>();
-builder.Services.AddScoped<IRefereeRepository, RefereeRepository>();           
-builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();     
-builder.Services.AddScoped<ITournamentTeamRepository, TournamentTeamRepository>(); 
+builder.Services.AddScoped<IRefereeRepository, RefereeRepository>();
+builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();
+builder.Services.AddScoped<ITournamentTeamRepository, TournamentTeamRepository>();
+builder.Services.AddScoped<ISponsorRepository, SponsorRepository>(); //Nuevo
+builder.Services.AddScoped<ITournamentSponsorRepository, TournamentSponsorRepository>(); //Nuevo
 
 
 // ── Services ──
 builder.Services.AddScoped<ITeamService, TeamService>();
 builder.Services.AddScoped<IPlayerService, PlayerService>();
-builder.Services.AddScoped<IRefereeService, RefereeService>();         
-builder.Services.AddScoped<ITournamentService, TournamentService>();     
+builder.Services.AddScoped<IRefereeService, RefereeService>();
+builder.Services.AddScoped<ITournamentService, TournamentService>();
+builder.Services.AddScoped<ISponsorService, SponsorService>(); // Nuevo
 
 
 // ── AutoMapper ──
