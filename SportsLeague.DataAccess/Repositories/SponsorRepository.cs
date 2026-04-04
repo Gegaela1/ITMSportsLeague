23df457fec95f295a235cdccaca13fd248473496
Andrea Espinosa <gespinol1210@gmail.com>
2026-04-03 19:53:16 -0500

    New entity created (Sponsor) and enum - implement Sponsor repository and queries - add Sponsor service - add Sponsor DTOs and mappings

---
+++ SportsLeague.DataAccess/Repositories/SponsorRepository.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Interfaces.Repositories;
+
+namespace SportsLeague.DataAccess.Repositories
+{
+    public class SponsorRepository
+        : GenericRepository<Sponsor>, ISponsorRepository
+    {
+        public SponsorRepository(LeagueDbContext context)
+            : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Sponsor>> GetByNameAsync(string name)
+        {
+            return await _dbSet
+                .Where(s => s.Name.ToLower() == name.ToLower())
+                .ToListAsync();
+        }
+    }
+}
+
+
+
