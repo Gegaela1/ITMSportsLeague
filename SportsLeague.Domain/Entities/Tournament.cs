effb61fdb56c53d3dfaedd981171dcadc7b79cd5
Andrea Espinosa <gespinol1210@gmail.com>
2026-04-03 20:23:50 -0500

    add missing TournamentSponsors navigation property to Tournament entity

--- SportsLeague.Domain/Entities/Tournament.cs
+++ SportsLeague.Domain/Entities/Tournament.cs
@@ -12,4 +12,8 @@ public class Tournament : AuditBase
 
     // Navigation Properties
     public ICollection<TournamentTeam> TournamentTeams { get; set; } = new List<TournamentTeam>();
+
+    public ICollection<TournamentSponsor> TournamentSponsors { get; set; }
+        = new List<TournamentSponsor>();
+
 }
