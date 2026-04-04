0bf763c55c565fb1b82229fff52b989fd4a8a38a
Andrea Espinosa <gespinol1210@gmail.com>
2026-04-03 22:30:06 -0500

    Add ISponsorService - SponsorService

---
+++ SportsLeague.Domain/Interfaces/Services/ISponsorService.cs
@@ -0,0 +1,19 @@
+﻿using SportsLeague.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.Interfaces.Services
+{
+    public interface ISponsorService
+    {
+        Task<Sponsor> CreateAsync(Sponsor sponsor);
+        Task<IEnumerable<Sponsor>> GetAllAsync();
+        Task<Sponsor?> GetByIdAsync(int id);
+        Task UpdateAsync(int id, Sponsor sponsor);
+        Task DeleteAsync(int id);
+
+        Task<TournamentSponsor> AddSponsorToTournamentAsync(int sponsorId, int tournamentId, decimal contractAmount);
+        Task<IEnumerable<TournamentSponsor>> GetTournamentsAsync(int sponsorId);
+    }
+}
---
+++ SportsLeague.Domain/Interfaces/Services/SponsorService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using SportsLeague.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.Interfaces.Services
+{
+    public class SponsorService : ISponsorService
+    {
+        private readonly ISponsorRepository _sponsorRepository;
+        private readonly ITournamentSponsorRepository _tournamentSponsorRepository;
+        private readonly ILogger<SponsorService> _logger;
+    }
+}
