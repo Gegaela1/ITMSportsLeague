0691fbabcdca476f4c249a7fa6cb8fc16c1179e6
Andrea Espinosa <gespinol1210@gmail.com>
2026-04-03 22:35:21 -0500

    Add Sponsor DTOs

--- SportsLeague.API/DTOs/Request/SponsorRequestDTO.cs
+++ SportsLeague.API/DTOs/Request/SponsorRequestDTO.cs
@@ -1,15 +1,14 @@
 ﻿using SportsLeague.Domain.Enums;
 
-namespace SportsLeague.API.DTOs.Response
-{    public class SponsorResponseDTO
+namespace SportsLeague.API.DTOs.Request
+{
+
+    public class SponsorRequestDTO
     {
-        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string ContactEmail { get; set; } = string.Empty;
         public string? Phone { get; set; }
         public string? WebsiteUrl { get; set; }
         public SponsorCategory Category { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
     }
 }
--- SportsLeague.Domain/Interfaces/Services/SponsorService.cs
+++ SportsLeague.Domain/Interfaces/Services/SponsorService.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
 using SportsLeague.Domain.Interfaces.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SportsLeague.Domain.Interfaces.Services
 {
